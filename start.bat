4b08d73313c694cb4cd69a6f7f3183031f57f5b0
Xylaa <ravensvenix@gmail.com>
2022-11-15 12:53:49 +0800

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,20 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"RDP WINDOWS SERVER 2019" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user administrator @admin /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo Success Installing RDP, RDP DEAD? BUILD AGAIN PLEASE!
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Please Check Your Ngrok Auth Token In Settings > Repository secret."
+echo Username: adminecho Password: @admin
+echo Please Login To Your RDP
+ping -n 10 127.0.0.1 >nul
