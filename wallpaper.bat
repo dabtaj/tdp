1fef88985dab340cffe43b6ad51b53fcb0088520
Cecilia Lucia Ximena <ravensvenix@gmail.com>
2023-01-16 03:10:52 +0800

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,5 +1,19 @@
 @echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+echo Looking for Admin Access!
+goto UACPrompt
+) else ( goto Meet Admin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:Meet Admin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
 CD /D "%~dp0"
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
 powershell -Command "& {Invoke-WebRequest https://drive.google.com/file/d/12DrLgL4IhcH1rBcBFZEo1xOoM19zOKXR -OutFile wallpaper.zip; Expand-Archive wallpaper.zip; Remove-Item wallpaper.zip}"
 @D:\a\wallpaper\wallpaper.exe D:\a\wallpaper\wallpaper.bgi /timer:0
 @RD /S /Q wallpaper
