ffd15fdd1f1fee1cf837ffcd2ce3375ea16cade6
Cecilia Lucia Ximena <ravensvenix@gmail.com>
2023-01-16 03:56:55 +0800

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -14,7 +14,7 @@ if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 pushd "%CD%"
 CD /D "%~dp0"
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /f /t REG_SZ /d a:/wallpaper.bmp
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /f /t REG_SZ /d d:/a/wallpaper.bmp
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
 start explorer.exe
